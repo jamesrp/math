462eec4953ed942f711e74d7525d0cf2daccf3bd
James Pfeiffer <jamesrpfeiffer@gmail.com>
2015-06-29 21:02:52 -0700

    Add example of recovering median from a single sample.

--- sample_median.py
+++ sample_median.py
@@ -23,6 +23,18 @@ def check(X, n):
 #  = 2.5.
 # An example run of check gives
 #  (49.562, 2.5781559999999915)
-print check(lambda: random.randrange(100), 1000)
+# print check(lambda: random.randrange(100), 1000)
 
+# Now look at the data from one run and predict the variance.
+def median_variance(xs):
+    s = sorted(xs)
+    c = len(xs)
+    a, b = int(c * .55), int(c * .45)
+    return s[c/2], (s[a] - s[b])**2 / (0.01 * 4 * c)
 
+# Sample output:
+# (2601, 27040.0)
+# (2462.734, 25236.599244000012)
+#xs = [random.randrange(100)**2 for _ in range(1000)]
+#print median_variance(xs)
+#print check(lambda: random.randrange(100)**2, 1000)
